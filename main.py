6610f40c01f8bfd4e41c3ca01735d3a0b470624b
fabrice-fouron <fouronfp05@gmail.com>
2021-05-13 09:29:20 -0400

    Linked both enter and index page together

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "test.db"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 class Notes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +16,13 @@ class Notes(db.Model):
 @app.route("/")  # Decorator to show the route of the page to render in the browser
 def index():
     return render_template('index.html')
+
+@app.route("/enternote", methods=['GET', 'POST'])
+def enternote():
+    return render_template('enternote.html')
+
+db.init_app(app)
+db.create_all()
 
 
 if __name__ == "__main__":
