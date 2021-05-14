c3ed720c7e63a5a4588c5fe0f20d6499c8d6f3f9
fabrice-fouron <fouronfp05@gmail.com>
2021-05-14 09:28:57 -0400

    The database works. The infos are stored on it successfully

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "test.db"
 # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 class Notes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, nullable=False)
-    note = db.Column(db.String(200), nullable=False)
-    favorite = db.Column(db.Boolean, nullable=False) 
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+    note = db.Column(db.String(2000), nullable=False)
+    favorite = db.Column(db.Boolean, default=True, nullable=False) 
 
 @app.route("/")  # Decorator to show the route of the page to render in the browser
 def index():
@@ -19,11 +19,13 @@ def index():
 
 @app.route("/enternote", methods=['GET', 'POST'])
 def enternote():
-    return render_template('enternote.html')
-
-db.init_app(app)
-db.create_all()
-
+    if request.method == "GET":  # Simply open the page
+        return render_template('enternote.html')
+    else:  # If the action is to post, do the following
+        note = Notes(note=request.form['enternote'])
+        db.session.add(note)
+        db.session.commit()
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
