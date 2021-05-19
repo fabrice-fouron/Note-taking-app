0734a0df2cea8db0e1c0723f778c93b614140e65
fabrice-fouron <fouronfp05@gmail.com>
2021-05-19 09:29:22 -0400

    Added the function to remove the file from the folder and remove it from the database

--- main.py
+++ main.py
@@ -1,31 +1,63 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 class Notes(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
-    note = db.Column(db.String(2000), nullable=False)
-    favorite = db.Column(db.Boolean, default=True, nullable=False) 
+    id = db.Column(db.Integer, primary_key=True)  # The unique of each note stored in the database
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow())  # The time the note was created or modified
+    title = db.Column(db.String(50), nullable=False)  # the title of the note
+
 
 @app.route("/")  # Decorator to show the route of the page to render in the browser
 def index():
     return render_template('index.html')
+
 
 @app.route("/enternote", methods=['GET', 'POST'])
 def enternote():
     if request.method == "GET":  # Simply open the page
         return render_template('enternote.html')
     else:  # If the action is to post, do the following
-        note = Notes(note=request.form['enternote'])
+        note = Notes(title=request.form['title'])
         db.session.add(note)
         db.session.commit()
-        return redirect("/")
+        save_note(request.form['title'], request.form['enternote'])  # Saves the note to the file
+        return redirect("/")  # Redirect to the main page
+
+
+@app.route("/shownotes")
+def shownotes():
+    return render_template('shownotes.html')
+
+
+@app.route("/display")
+def display():
+    return render_template('display.html')
+
+@app.route('/update')
+def update_note(title):
+    # newFile =  # Going to create a new file to replace the previous one
+    f = open(f"./notes/{title}.txt", "w")
+
+
+def save_note(title, text):  # Save the notes in the notes folder and in the database
+    f = open(os.path.join("./notes/", f"{title}.txt"), "w")
+    f.write(text)
+    note = Notes(title=request.form['title'])
+    db.session.add(note)
+    
+
+def delete_note(title):  # Remove the note from the database and from the notes folder
+    db.session.delete(Notes.query.filter_by(username='peter'))
+    os.remove(f"./notes/{title}.txt")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
