18176df011c3666d69307fbbf7c2da5ca0f9b23b
fabrice-fouron <fouronfp05@gmail.com>
2021-06-17 08:24:36 -0400

    Final version of project

--- main.py
+++ main.py
@@ -122,4 +122,4 @@ def no_underscore(name):  # Replace the underscore in the filename to a space
   return name
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
